ce8535759879a391aa309df5fe3ccbebfd4ccf68
Rawle Becerril <rawlesxm@gmail.com>
2023-05-17 18:30:20 -0400

    edited bio and adjusted email form length

--- src/components/bio.js
+++ src/components/bio.js
@@ -22,9 +22,9 @@ return (<>
         <h3>About Me</h3>
         <br></br>
         {/* {aboutus.bio} */}
-        <h5>Hey there! My name is Rawle Becerril and after nearly a decade of professional and leadership experience in the radio and music industries, I've decided to set my sights on a whole new challenge in the world of Software Development. <br></br><br></br>
+        <h5>Hey there! My name is Rawle Becerril and after nearly a decade of professional and leadership experience in radio at entertainment titan SiriusXM, I've decided to set my sights on a whole new challenge in the world of Software Development. <br></br><br></br>
         
-        I am a highly adaptable Full-Stack web developer building creative, responsive, and highly-functional applications using HTML, CSS, and JavaScript with React. When I'm not building, I enjoy cooking anything from shrimp tacos to Beef Wellington, gaming, and making and performing electronic music!</h5>
+        I am a highly adaptable Full-Stack web developer building creative, responsive, and highly-functional applications using React, NextJS, Javascript & Typescript. When I'm not building, I enjoy flexing my culinary skills in the kitchen, gaming, and playing and performing music!</h5>
     </BioDescriptionContainer>
     <br></br>
 </BioContainer>
